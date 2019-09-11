9752d5a089f59f698b9babf79b4eae29a7e4f597
David Wilmot <david.wilmot@ed.ac.uk>
2019-09-11 13:26:21 +0100

    Stop allowing keypresses all the way through. Min 1 seconds between each annotation.

--- src/App.svelte
+++ src/App.svelte
@@ -19,7 +19,7 @@
       firebase.initializeApp(firebaseConfig);
 
     let db = firebase.firestore();
-    let db_collection = "sentence_annotations_sandbox";
+    let db_collection = "sentence_annotations_test";
 
     $: valid_story = true;
 
@@ -31,6 +31,7 @@
     let hit_id = null;
     let turk_submit_to = null;
     let worker_id = null;
+    let min_duration = 1000;
 
     let annotations_lookup = new Map();
     for (const s of annotations_source.stories) {
@@ -40,7 +41,8 @@
     import { writable } from 'svelte/store';
     export const story_annotations = writable([]);
 
-	$: active_story_id = -1
+
+	$: active_story_id = -1;
 	$: code = -1;
 	$: last_choice = 0;
 	$: active_story_complete = false;
@@ -58,26 +60,29 @@
 
         let duration = new Date().getTime() - start_timer;
 
-        let annotation_result_map = {"story_id": active_story_id, "suspense": choice, "duration_milliseconds": duration};
-        annotation_result_map["sentence_num"] = active_sentence["sentence_num"];
-        annotation_result_map["sentence_id"] = active_sentence["sentence_id"];
-        annotation_result_map["sentence_len"] = active_sentence["sentence_len"];
+        if (duration > min_duration){
 
-         if (active_story_complete === false) {
-            console.log(`Annotations: ${annotation_result_map}`);
-            story_annotations.update(n => n.concat([annotation_result_map]));
-         }
+            let annotation_result_map = {"story_id": active_story_id, "suspense": choice, "duration_milliseconds": duration};
+            annotation_result_map["sentence_num"] = active_sentence["sentence_num"];
+            annotation_result_map["sentence_id"] = active_sentence["sentence_id"];
+            annotation_result_map["sentence_len"] = active_sentence["sentence_len"];
 
-        if (active_sentence_index + 1 === active_story_sentences.length) {
-            active_story_complete = true;
-            workflow_state = "SUMMARY"
+             if (active_story_complete === false) {
+                console.log(`Annotations: ${annotation_result_map}`);
+                story_annotations.update(n => n.concat([annotation_result_map]));
+             }
+
+            if (active_sentence_index + 1 === active_story_sentences.length) {
+                active_story_complete = true;
+                workflow_state = "SUMMARY"
+            }
+
+            active_sentence_index = Math.min(active_sentence_index + 1, active_story_sentences.length - 1);
+            active_sentence = active_story_sentences[active_sentence_index]
+            last_choice = choice;
+
+            start_timer = new Date().getTime();
         }
-
-        active_sentence_index = Math.min(active_sentence_index + 1, active_story_sentences.length - 1);
-        active_sentence = active_story_sentences[active_sentence_index]
-        last_choice = choice;
-
-        start_timer = new Date().getTime();
 
     }
 
@@ -106,6 +111,9 @@
     }
 
     function undoAnnotation() {
+
+    let duration = new Date().getTime() - start_timer;
+    if (duration > min_duration){
          if (active_story_complete === false) {
                     story_annotations.update(n => {
                         active_sentence_index -= 1
@@ -113,6 +121,10 @@
                         return n.slice(0, -1);
                     })
                  }
+        }
+
+        start_timer = new Date().getTime();
+
     }
 
     function post(path, params, method='post') {
@@ -211,7 +223,7 @@
         }
      }
 
-    function handleKeydown(event) {
+    function handleKey(event) {
         if (workflow_state === "ANNOTATE") {
             if  (event.key === "a" || event.key === "A" ) {
                 sentenceBigDecrease()
@@ -255,7 +267,7 @@
 
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
 
-<svelte:window on:keydown={handleKeydown}/>
+<svelte:window on:keyup={handleKey}/>
 
 {#if workflow_state === "INVALID_STORY"}
 <div id="sentence">
@@ -267,6 +279,7 @@
 
 {:else if workflow_state === "INSTRUCTIONS"}
 <div id="sentence">
+
 
       <button on:click={startStoryAnnotation}>Start</button>
 
