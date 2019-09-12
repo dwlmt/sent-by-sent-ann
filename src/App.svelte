c1fadb7b0cea4eca8769aa24e2afd3727e556896
David Wilmot <david.wilmot@ed.ac.uk>
2019-09-12 13:54:49 +0100

    Further refinements to instructions.

--- src/App.svelte
+++ src/App.svelte
@@ -19,7 +19,7 @@
       firebase.initializeApp(firebaseConfig);
 
     let db = firebase.firestore();
-    let db_collection = "sentence_annotations_test";
+    let db_collection = "sentence_annotations_sandbox";
 
     $: valid_story = true;
 
@@ -31,7 +31,7 @@
     let hit_id = null;
     let turk_submit_to = null;
     let worker_id = null;
-    let min_duration = 1000;
+    let min_duration = 500;
 
     let annotations_lookup = new Map();
     for (const s of annotations_source.stories) {
@@ -260,14 +260,12 @@
 
       <button on:click={startStoryAnnotation}>Start</button>
 
-    <h2>Instructions</h2>
-    <h3>Overview</h3>
-    <h4>Sentence by Sentence</h4>
-    <p>  You will read a short story and for each sentence be asked to assess how the drama tension increases, decreases or stays the same. Each story will take an estimated
-    <emph>5-7 minutes</emph>.Judge each sentence on how the dramatic tension has changed over after reading a new sentence as felt by the main characters in the story, not
+    <h2>Story Reading Sentence by Sentence</h2>
+    <p>  You will read a short story and for each sentence be asked to assess how the dramatic tension increases, decreases or stays the same. Each story will take an estimated
+    <emph>5-7 minutes</emph>.Judge each sentence on how the dramatic tension has changed over as felt by the main characters in the story, not
     what you as a reader feel. This tension is the excitement or anxiousness over what will happen to the characters next, it is antipation.</p>
     <p>
-     <strong>An Example:</strong> Take a dramatic moment in a story main character that needs to walk along a dangerous cliff path. When the character first realises they will encounter danger the tension
+     <strong>An Example:</strong> Take a dramatic moment in a story main such as a character that needs to walk along a dangerous cliff path. When the character first realises they will encounter danger the tension
      will rise. As they are on the cliff ledge then tension will increase further. Other details such as falling rocks or slips will increase the tension further to a peak. When the cliff edge
      has been navigated safely the tension will drop. The pattern will be the same with a dramatic event such as a fight, argument, accident, romantic moment, where the tension will rise to a peak
      and then fall away as the tension is resolved.
@@ -275,13 +273,11 @@
 
      <p>Increasing levels of each of the following increase the level of dramatic tension:</p>
         <ul>
-         <li><strong>Uncertainty:</strong> How uncertain are the characters involved about what will happen next. As a reader you may be sure what will happen but
-         judge the change in the tension based on how the characters perceive the situation. Put yourself in the characters shoes. </li>
+         <li><strong>Uncertainty:</strong> How uncertain are the characters involved about what will happen next? Put yourself in the characters shoes; judge the change in the tension based on how the characters perceive the situation.  </li>
          <li><strong>Significance:</strong>How significant are the consequences of what will happen to the central characters of the story.</li>
         </ul>
-    <p>Moments that combine uncertainty for the characters with a high significance or consequences will increase the tension. Those that reduce the uncertainty or significance will decrease the tension.
-    Keyboard shortcuts are shown in brackets and will enable you to speed up the annotation process.
-    The scale has 5 choices, two to reduce the tension, one to increase and one the same:
+    <p>Keyboard shortcuts are shown in brackets and will enable you to speed up the annotation process.
+    The scale has 5 choices:
     </p>
     <ul>
       <li><strong>Big Decrease (A):</strong> A sudden dramatic decrease in dramatic tension of the situation. In the cliff example the person reaching the other side safely.</li>
@@ -291,55 +287,60 @@
       <li><strong>Big Increase (L):</strong> A more sudden dramatic increase such as an argument. The cliff walker suddenly slips and falls. </li>
        </ul>
 
-    <h4>Review Questions</h4>
-    <p>At the end are a review questions to assess the story overall:</p>
-    <ul>
-      <li><strong>Summary: </strong> Write a short one or two sentence summary of what the story is about. This should mention the main character(s),
-      plot points, goals or obstacles that the characters overcame in the story.</li>
-      <li><strong>Review:</strong> Write a couple of sentences reviewing the story saying what you like or don't like about it and why. For either mention if it is the topic, characters, plot,
-      etc that draw you into the story or find uninteresting</li>
-      <li><strong>Rating:</strong> A 5 point scale from very uninteresting to very interesting. Just go with your own judgement on how interesting the story is.</li>
-    </ul>
-    <h3>Examples</h3>
-    <p>These are some annotated extracts from the stories for guidance. </p>
+    <h4>Example Story</h4>
 
-    <h4>Foxhole</h4>
+    <p>An example story with guidance answers:</p>
 
-    <table>
-    <tr><th>Judgement</th><th>Sentence Number</th><th>Text</th></tr>
-    <tr><td>NA</td><td>1</td><td>Clancy Marguerian , 154 , private first class of the 150 + army , sits in his foxhole .</td></tr>
-    <tr><td>Increase</td><td>2</td><td>Tired cold , wet and hungry , the only thing preventing him from laying down his rifle and walking towards the enemy lines in surrender is the knowledge that however bad he has it here , life as a 50 - 100 POW is surely much worse .</td></tr>
-    <tr><td>Increase</td><td>3</td><td>He &#39;s fighting to keep his eyes open and his rifle ready when the mortar shells start landing near him .</td></tr>
-    <tr><td>Same</td><td>4</td><td>He hunkers lower .</td></tr>
-    <tr><td>Increase</td><td>5</td><td>After a few minutes under the barrage , Marguerian hears hurried footsteps , a grunt , and a thud as a soldier leaps into the foxhole .</td></tr>
-    <tr><td>Same</td><td>6</td><td>The man &#39;s uniform is tan , he must be a 50 - 100 .</td></tr>
-    <tr><td>Big Increase</td><td>7</td><td>The two men snarl and grab at eachother , grappling in the small foxhole .</td></tr>
-    <tr><td>Same</td><td>8</td><td>Abruptly , their faces come together .</td></tr>
-    <tr><td>Decrease</td><td>9</td><td>`` Clancy ? &#39;&#39;</td></tr>
-    <tr><td>Decrease</td><td>10</td><td>`` Rob ? &#39;&#39;</td></tr>
-    <tr><td>Big Decrease</td><td>11</td><td>Rob Hall , 97 , Corporal in the 50 - 100 army grins , as the situation turns from life or death struggle , to a meeting of two college friends .</td></tr>
-   </table>
+     <table>
+        <tr><th>Judgement</th><th>Sentence Number</th><th>Text</th></tr>
+        <tr><td>NA</td><td>0</td><td>Clancy Marguerian , 154 , private first class of the 150 + army , sits in his foxhole .</td></tr>
+        <tr><td>Increase</td><td>1</td><td>Tired cold , wet and hungry , the only thing preventing him from laying down his rifle and walking towards the enemy lines in surrender is the knowledge that however bad he has it here , life as a 50 - 100 POW is surely much worse .</td></tr>
+        <tr><td>Increase</td><td>2</td><td>He &#39;s fighting to keep his eyes open and his rifle ready when the mortar shells start landing near him .</td></tr>
+        <tr><td>Same</td><td>3</td><td>He hunkers lower .</td></tr>
+        <tr><td>Increase</td><td>4</td><td>After a few minutes under the barrage , Marguerian hears hurried footsteps , a grunt , and a thud as a soldier leaps into the foxhole .</td></tr>
+        <tr><td>Same</td><td>5</td><td>The man &#39;s uniform is tan , he must be a 50 - 100 .</td></tr>
+        <tr><td>Big Increase</td><td>6</td><td>The two men snarl and grab at eachother , grappling in the small foxhole .</td></tr>
+        <tr><td>Same</td><td>7</td><td>Abruptly , their faces come together .</td></tr>
+        <tr><td>Decrease</td><td>8</td><td>`` Clancy ? &#39;&#39;</td></tr>
+        <tr><td>Decrease</td><td>9</td><td>`` Rob ? &#39;&#39;</td></tr>
+        <tr><td>Big Decrease</td><td>10</td><td>Rob Hall , 97 , Corporal in the 50 - 100 army grins , as the situation turns from life or death struggle , to a meeting of two college friends .</td></tr>
+        <tr><td>Decrease</td><td>11</td><td>He lets go of Marguerian &#39;s collar .</td></tr>
+        <tr><td>Same</td><td>12</td><td>`` Holy shit Clancy , you &#39;re the last person I expected to see here &#39;&#39;</td></tr>
+        <tr><td>Same</td><td>13</td><td>`` Yeah &#39;&#39; `` Shit man , I did n&#39;t think I &#39;d ever see &#39;</td></tr>
+        <tr><td>Same</td><td>14</td><td>Mr .</td></tr>
+        <tr><td>Decrease</td><td>15</td><td>volunteers every saturday morning at the food shelf &#39; , not after The Reorganization at least &#39;&#39;</td></tr>
+        <tr><td>Same</td><td>17</td><td>Yeah Rob , it is something is n&#39;t it &#39;&#39;</td></tr>
+        <tr><td>Decrease</td><td>18</td><td>`` Man , I &#39; m sorry</td></tr>
+        <tr><td>Same</td><td>19</td><td>I tried to kill you there ,</td></tr>
+        <tr><td>Same</td><td>20</td><td>hey , I heard you guys were out of food , here , you can share my dinner &#39;&#39; Clancy marvels , even after all this : The Reorganization , the coalitions , the war , Rob is still his old , chatty self .</td></tr>
+        <tr><td>Same</td><td>21</td><td>The two men sit , Rob chatting away , Clancy forcing out pleasantries .</td></tr>
+        <tr><td>Same</td><td>22</td><td>They pass Rob &#39;s rations between them .</td></tr>
+        <tr><td>Increase</td><td>23</td><td>`` Clancy my man , I heard a group of terrorist 5 &#39;s took have formed some kind of cult , and they &#39;re rallying all the &lt; 50 in their own coalition &#39;&#39;</td></tr>
+        <tr><td>Same</td><td>24</td><td>`` Oh yeah ? &#39;&#39;</td></tr>
+        <tr><td>Decrease</td><td>25</td><td>`` Yeah , I mean , that sucks and everything , cause those are some scary dudes , but I heard that there &#39;s going to be a truce between our countries in a few days , why do n&#39;t we just hang out here , pretty soon we wo n&#39;t even be enemies anymore ! &#39;&#39;</td></tr>
+        <tr><td>Same</td><td>26</td><td>`` Yeah , Rob , that sounds like a plan &#39;&#39;</td></tr>
+        <tr><td>Increase</td><td>27</td><td>`` Man , I &#39; m so glad I found you again , in a few days , this war will be over , and things will be cool between us and , hey , remember Sarah ?</td></tr>
+        <tr><td>Same</td><td>28</td><td>I heard she &#39;s a 151 , maybe I &#39;ll look her up , I &#39;ll be sure to visit you too once I can get a pass to sector 150 - 155 , it &#39;ll probably be tough though , even before the war</td></tr>
+        <tr><td>Same</td><td>29</td><td>, you had to do sooo much paperwork to be allowed to visit , I wonder if passes will even be reinstated after the truce ends , hey , did I ever tell you about the time ... &#39;&#39;</td></tr>
+        <tr><td>Increase</td><td>30</td><td>Rob babbles as he dozes off , grinning up at Clancy .</td></tr>
+        <tr><td>Big Increase</td><td>31</td><td>When Clancy is sure that his friend is asleep , he slits Rob &#39;s throat with his bayonet .</td></tr>
+        <tr><td>Decrease</td><td>32</td><td>Clancy climbs out of the foxhole , and stumbles his way back to battalion HQ .</td></tr></table>
 
-    <h4>Lake Murder</h4>
-
-    <table>
-    <tr><td>NA</td><td>1</td><td>`` Reading my book , as I wanted to see if there were fish in the cold water at the top . &#39;&#39;</td></tr>
-    <tr><td>Same</td><td>2</td><td>He stepped to the rail and leaned over .</td></tr>
-    <tr><td>Increase</td><td>3</td><td>He deepened his voice and saw from the corner of his eye as the officers got curious and moved towards him .</td></tr>
-    <tr><td>Increase</td><td>4</td><td>`` But what I saw - &#39;&#39; , he made a short pause while the officers leaned over the rail to see into the water .</td></tr>
-    <tr><td>Increase</td><td>5</td><td>`` - scared me to DEATH ! &#39;&#39;</td></tr>
-    <tr><td>Big Increase</td><td>6</td><td>With the last word he swung the meat cleaver , which sunk deep in the man &#39;s head .</td></tr>
-    <tr><td>Increase</td><td>7</td><td>The body flinched and fell into the water , the woman jumped back , terror in her eyes , and tried to get her pistol from the belt .</td></tr>
-    <tr><td>Big Increase</td><td>8</td><td>But too late, over and over again he hacked her flesh .</td></tr>
-    <tr><td>Same </td><td>9</td><td>She did n&#39;t even scream .</td></tr>
-    <tr><td>Same</td><td>10</td><td>The horror had shut her mouth .</td></tr>
-    <tr><td>Decrease</td><td>11</td><td>As the pain faded away and she felt very tired , she sank back so that her upper body was hanging over the rail .</td></tr>
-    <tr><td>Big Decrease</td><td>12</td><td>Her last look was towards the grey sky , where the last snow began to fall .</td></tr></table>
 
 </div>
 {:else if workflow_state === "SUMMARY"}
 <div id="sentence">
-    <h2>Summary</h2>
+    <h2>Review Questions</h2>
+
+        <p>The following review questions are assess the story overall:</p>
+        <ul>
+          <li><strong>Summary: </strong> Write a short one or two sentence summary of what the story is about. This should mention the main character(s),
+          plot points, goals or obstacles that the characters overcame in the story.</li>
+          <li><strong>Review:</strong> Write a couple of sentences reviewing the story saying what you like or don't like about it and why. For either mention if it is the topic, characters, plot,
+          etc that draw you into the story or find uninteresting</li>
+          <li><strong>Rating:</strong> A 5 point scale from very uninteresting to very interesting. Just go with your own judgement on how interesting the story is.</li>
+        </ul>
+
     <p>
     <h4>Please write a summary of the story in one or two sentences.</h4>
     <form onsubmit="event.preventDefault();">
