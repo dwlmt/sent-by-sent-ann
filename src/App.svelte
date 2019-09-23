b1eb300505af95aa701d195334fe7e516baa0e02
David Wilmot <david.wilmot@ed.ac.uk>
2019-09-23 16:10:56 +0100

    Revert to a small millsecond delay between story sentences.

--- src/App.svelte
+++ src/App.svelte
@@ -37,7 +37,7 @@
     let hit_id = null;
     let turk_submit_to = null;
     let worker_id = null;
-    let min_duration_per_word = 100;
+    let min_duration = 50;
 
     let annotations_lookup = new Map();
     for (const s of annotations_source.stories) {
@@ -97,7 +97,7 @@
 
         let duration = new Date().getTime() - start_timer;
 
-        if (duration > (min_duration_per_word * active_sentence["sentence_len"])){
+        if (duration > (min_duration)){
 
             let annotation_result_map = {"story_id": active_story_id, "suspense": choice, "duration_milliseconds": duration};
             annotation_result_map["sentence_num"] = active_sentence["sentence_num"];
